8b648f6ad58e505238733dcabf153accd5e4079d
Alena <89519703610alena@gmail.com>
2024-09-07 13:53:39 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,15 @@
+immutable_var = 1, 2, 3, 'python'
+print(immutable_var)
+#immutable_var = [1] = 0 # Значение кортежа изменить нельза. Можно только добавить элементы с помощью сложения или умножения.
+immutable_var = ( 1, 2, 3, 'pytoh') + ( 5, 6 )
+print( immutable_var)
+immutable_var_1 = 1, 2, 3,
+print( immutable_var_1)
+immutable_var_1 = (1, 2, 3 ) * (2)
+print(immutable_var_1)
+
+immutable_var = ['дом', 'стол', 'книга' ]
+print(immutable_var)
+print( immutable_var [0])
+immutable_var [0] = 'комната'
+print( immutable_var)
